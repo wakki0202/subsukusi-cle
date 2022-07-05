57393dffeac9ce777035b56b0d9389b175f22430
wakki <ytk0202@docomo.ne.jp>
2022-07-05 14:04:56 +0900

    first commit

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -6,11 +6,18 @@ import {
   DisplayText,
   TextStyle,
   Button,
+  RadioButton,
+  Stack,
+  Subheading,
+  FormLayout, TextField,
+  Select, Frame, Navigation, Layout
 } from "@shopify/polaris";
+import { HomeMinor, OrdersMinor, ProductsMinor } from "@shopify/polaris-icons";
 import { Toast, useAppBridge } from "@shopify/app-bridge-react";
 import { gql, useMutation } from "@apollo/client";
 
 import { userLoggedInFetch } from "../App";
+
 
 const PRODUCTS_QUERY = gql`
   mutation populateProduct($input: ProductInput!) {
@@ -45,21 +52,110 @@ export function ProductsCard() {
     />
   );
 
+
+
+  const [value, setValue] = useState('disabled');
+
+  const handleChange = useCallback(
+    (_checked, newValue) => setValue(newValue),
+    [],
+  );
+
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('You clicked submit.');
+  }
+
+  const [selected, setSelected] = useState("today");
+
+  const handleSelectChange = useCallback((value) => setSelected(value), []);
+
+  const options = [
+    { label: "日", value: "日" },
+    { label: "週", value: "週" },
+    { label: "月", value: "月" },
+    { label: "年", value: "年" },
+  ];
+
   return (
     <>
+    
+      <Layout>
+      <Layout.Section>
+      <Frame>
+        <Navigation location="/">
+          <Navigation.Section
+            items={[
+              {
+                url: "/",
+                label: "Home",
+                icon: HomeMinor,
+              },
+              {
+                url: "/path/to/place",
+                label: "Orders",
+                icon: OrdersMinor,
+                badge: "15",
+              },
+              {
+                url: "/path/to/place",
+                label: "Products",
+                icon: ProductsMinor,
+              },
+            ]}
+          />
+        </Navigation>
+        </Frame>
+      </Layout.Section>
+        <Layout.Section secondary>
       {toastMarkup}
-      <Card title="Product Counter" sectioned>
+      <Heading element="h2">プランの作成</Heading>
+      <Heading element="h2">プランのグループ</Heading>
+      <Subheading>プラングループ単位で商品に設定します。</Subheading>
+
+      <Card title="プランの作成" sectioned>
+        <FormLayout>
+          <TextField label="プラングループタイトル" onChange={() => { }} autoComplete="off" />
+          <Subheading>管理画面上で表示されるタイトルを入力してください</Subheading>
+
+          <TextField
+            type="email"
+            label="公開タイトル"
+            onChange={() => { }}
+            autoComplete="email"
+          />
+          <Subheading>商品詳細ページ上でお客様に表示される定期購買のプラン名を入力してください。</Subheading>
+        </FormLayout>
+        
+          <Stack vertical>
+            <RadioButton
+              label="１ヶ月ごと"
+              checked={value === 'disabled'}
+              id="disabled"
+              name="accounts"
+              onChange={handleChange}
+            />
+            <RadioButton
+              label="３ヶ月ごと"
+              id="optional"
+              name="accounts"
+              checked={value === 'optional'}
+              onChange={handleChange}
+            />
+          </Stack>
+
+          
+          <Button onClick={handleClick}>
+            期間を変更する
+        </Button>
         <TextContainer spacing="loose">
-          <p>
-            Sample products are created with a default title and price. You can
-            remove them at any time.
-          </p>
           <Heading element="h4">
             TOTAL PRODUCTS
             <DisplayText size="medium">
               <TextStyle variation="strong">{productCount}</TextStyle>
             </DisplayText>
           </Heading>
+
           <Button
             primary
             loading={loading}
@@ -82,8 +178,55 @@ export function ProductsCard() {
           >
             Populate 5 products
           </Button>
+          
+
         </TextContainer>
       </Card>
+      <Card title="プランの作成" sectioned>
+        <FormLayout>
+          <TextField label="プラン" onChange={() => { }} autoComplete="off" />
+          <Subheading>購入の間隔がわかるように書くことをおすすめしています。</Subheading>
+
+        </FormLayout>
+
+        <FormLayout label="">
+          購入の間隔
+          <FormLayout.Group >
+            
+            <TextField
+              type="number"
+              label=""
+              onChange={() => { }}
+              autoComplete="off"
+            />
+            <TextField
+              onChange={() => { }}
+              autoComplete="off"
+            />
+          </FormLayout.Group>
+        </FormLayout>
+
+
+      
+
+        <Select
+          label="Date range"
+          options={options}
+          onChange={handleSelectChange}
+          value={selected}
+        />
+
+
+        <Button onClick={handleClick}>
+          期間を変更する
+        </Button>
+        <TextContainer spacing="loose">
+       
+
+        </TextContainer>
+        </Card>
+        </Layout.Section>
+    </Layout>
     </>
   );
 }
@@ -96,135 +239,10 @@ function randomTitle() {
 }
 
 const ADJECTIVES = [
-  "autumn",
-  "hidden",
-  "bitter",
-  "misty",
-  "silent",
-  "empty",
-  "dry",
-  "dark",
-  "summer",
-  "icy",
-  "delicate",
-  "quiet",
-  "white",
-  "cool",
-  "spring",
-  "winter",
-  "patient",
-  "twilight",
-  "dawn",
-  "crimson",
-  "wispy",
-  "weathered",
-  "blue",
-  "billowing",
-  "broken",
-  "cold",
-  "damp",
-  "falling",
-  "frosty",
-  "green",
-  "long",
-  "late",
-  "lingering",
-  "bold",
-  "little",
-  "morning",
-  "muddy",
-  "old",
-  "red",
-  "rough",
-  "still",
-  "small",
-  "sparkling",
-  "throbbing",
-  "shy",
-  "wandering",
-  "withered",
-  "wild",
-  "black",
-  "young",
-  "holy",
-  "solitary",
-  "fragrant",
-  "aged",
-  "snowy",
-  "proud",
-  "floral",
-  "restless",
-  "divine",
-  "polished",
-  "ancient",
-  "purple",
-  "lively",
-  "nameless",
+  "まぬけな",
+  "りゅうき"
 ];
 
 const NOUNS = [
-  "waterfall",
-  "river",
-  "breeze",
-  "moon",
-  "rain",
-  "wind",
-  "sea",
-  "morning",
-  "snow",
-  "lake",
-  "sunset",
-  "pine",
-  "shadow",
-  "leaf",
-  "dawn",
-  "glitter",
-  "forest",
-  "hill",
-  "cloud",
-  "meadow",
-  "sun",
-  "glade",
-  "bird",
-  "brook",
-  "butterfly",
-  "bush",
-  "dew",
-  "dust",
-  "field",
-  "fire",
-  "flower",
-  "firefly",
-  "feather",
-  "grass",
-  "haze",
-  "mountain",
-  "night",
-  "pond",
-  "darkness",
-  "snowflake",
-  "silence",
-  "sound",
-  "sky",
-  "shape",
-  "surf",
-  "thunder",
-  "violet",
-  "water",
-  "wildflower",
-  "wave",
-  "water",
-  "resonance",
-  "sun",
-  "wood",
-  "dream",
-  "cherry",
-  "tree",
-  "fog",
-  "frost",
-  "voice",
-  "paper",
-  "frog",
-  "smoke",
-  "star",
+  "坂本"
 ];
